e1cbdbc4af9dedcb61963b55bbe4787c9f13c11a
rzadp <przemek@ethworks.io>
2019-05-02 18:52:58 +0200

    Remote image.

--- badminton/docker_and_train.sh
+++ badminton/docker_and_train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ROOT_DIR="$(realpath ..)"
 
-docker run -it -v "$(realpath ..)":/root/host badminton_image:latest /bin/bash -c "cd host/badminton && ./train.sh"
+docker run -it -v "$(realpath ..)":/root/host eu.gcr.io/badminton/badminton_image /bin/bash -c "cd host/badminton && ./train.sh"
 echo "Done."
