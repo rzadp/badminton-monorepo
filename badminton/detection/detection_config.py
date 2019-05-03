bd4ae2e6f2422f73ea1ab7e4e33bf17b592ac06d
rzadp <przemek@ethworks.io>
2019-05-03 13:07:59 +0200

    Added detecion config.

---
+++ badminton/detection/detection_config.py
@@ -0,0 +1,15 @@
+import os
+ROOT_DIR = os.path.abspath("../../Mask_RCNN")
+from common.badminton_config import BadmintonConfig
+
+class DetectionConfig(BadmintonConfig):
+    IMAGES_PER_GPU = 1
+    GPU_COUNT = 1
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+    USE_MINI_MASK = False
+
+    MASK_SHAPE = [28, 28]
+    # MASK_SHAPE = [56, 56]
