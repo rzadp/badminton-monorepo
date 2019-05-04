41ec6bcd82d3e671292b5b333ac4e7cc1732d7aa
rzadp <przemek@ethworks.io>
2019-05-04 12:46:36 +0200

    Removing output first.

--- badminton/detection/docker_detect.sh
+++ badminton/detection/docker_detect.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rm -f ./output.png ./output*.txt
 docker run -it -v "$(realpath ../..)":/root/host eu.gcr.io/badminton/badminton_image /bin/bash -c "cd host/badminton/detection && python3 ./detect.py"
