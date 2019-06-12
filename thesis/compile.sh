be9a7214b001f8e47203442386a77048f6f80388
rzadp <przemek@ethworks.io>
2019-06-12 17:16:42 +0200

    Introduced plantuml into compile script

--- thesis/compile.sh
+++ thesis/compile.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
+set -e
+
 cd title_page
 xelatex strona_tytulowa-jeden-autor.tex
 cd ..
 
 echo "...::: Compiled title page :::..."
 
+java -jar $PLANTUML_JAR ./content/chapters/**/*.plantuml
 
-echo "...::: TODO: Exported diagrams :::..."
+echo "...::: Exported diagrams :::..."
 
 pdflatex main.tex
 pdflatex main.tex # twice because of the table of contents bug
