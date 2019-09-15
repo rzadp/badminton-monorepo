80b7183767be2f01d4a49f96d1ddc0caada677eb
rzadp <przemek@ethworks.io>
2019-09-15 20:15:39 +0200

    Bash compatible way of compiling png

--- thesis/scripts/mermaid.sh
+++ thesis/scripts/mermaid.sh
@@ -5,4 +5,6 @@ cd $(dirname $0)
 command -v node || { echo "node not found"; exit -1; }
 
 cd ..
-for file in ./**/*.mmd; do ../node_modules/.bin/mmdc -i $file -o $(basename "$file" .mmd).png; done
+find . -name '*.mmd' | while read file; do
+  ./node_modules/.bin/mmdc -i $file -o $(basename "$file" .mmd).png
+done
