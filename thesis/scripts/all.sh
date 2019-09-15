063638038b9084549603d606a264eab1bc1f647f
rzadp <przemek@ethworks.io>
2019-09-15 19:28:04 +0200

    Compile thesis

--- thesis/scripts/all.sh
+++ thesis/scripts/all.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/bin/bash
 set -e
 cd $(dirname $0)
 
@@ -13,5 +13,5 @@ echo "...::: Exported diagrams :::..."
 
 cd ..
 max_in_open=32 pdflatex -interaction=nonstopmode -halt-on-error -synctex=1 main.tex
-max_in_open=32 pdflatex -interaction=nonstopmode -halt-on-error -synctex=1 main.tex # twice because of the table of contents bug
+!! # twice because of the table of contents bug
 echo "...::: Compiled thesis :::..."
