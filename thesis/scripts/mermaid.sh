9790ea0ff75e81ff5b7781748487b78c043548c0
rzadp <przemek@ethworks.io>
2019-09-15 19:39:51 +0200

    Node

--- thesis/scripts/mermaid.sh
+++ thesis/scripts/mermaid.sh
@@ -2,9 +2,7 @@
 set -e
 cd $(dirname $0)
 
-export NVM_DIR="/Users/przemek/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
-nvm use default
+command -v node || { echo "node not found"; exit -1; }
 
 cd ..
 for file in ./**/*.mmd; do ../node_modules/.bin/mmdc -i $file -o $(basename "$file" .mmd).png; done
