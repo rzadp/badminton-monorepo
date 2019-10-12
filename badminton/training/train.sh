bf60215eadae5c80b49d4f3bdd06af07805e7415
rzadp <rzadp@student.mini.pw.edu.pl>
2019-10-12 20:18:39 +0200

    case name

--- badminton/training/train.sh
+++ badminton/training/train.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 cd $(dirname $0)
 set -e
-
-source cases/example.env
-
 cd ..
-python3 ./badminton.py train \
---dataset=./datasets/$DATASET \
---weights=coco \
---epochs=$EPOCHS \
---use_multiprocessing=False \
---steps_per_epoch=1 \
---validation_steps=1 \
---logs=./training/logs \
---case=$CASE
+
+for f in training/cases/*.env
+do
+  CASE=$(basename "$f" .env)
+  echo "Running for case: $CASE"
+  source "$f"
+
+  python3 ./badminton.py train \
+  --dataset=./datasets/$DATASET \
+  --weights=coco \
+  --epochs=$EPOCHS \
+  --use_multiprocessing=False \
+  --steps_per_epoch=1 \
+  --validation_steps=1 \
+  --logs=./training/logs \
+  --case=$CASE
+done
