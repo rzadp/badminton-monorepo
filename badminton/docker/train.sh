b9364c955d973549ade12281b1bae88af37373d6
rzadp <przemek@ethworks.io>
2019-10-14 22:02:22 +0200

    CI

--- badminton/docker/train.sh
+++ badminton/docker/train.sh
@@ -4,6 +4,6 @@ set -e
 ROOT_DIR="$(realpath ../..)"
 
 docker build -t badminton_image .
-docker run -it -v "$ROOT_DIR":/mono badminton_image /bin/bash -c "cd /mono/badminton/training && ./train.sh"
+docker run -e CI -it -v "$ROOT_DIR":/mono badminton_image /bin/bash -c "cd /mono/badminton/training && ./train.sh"
 
 echo "Done."
