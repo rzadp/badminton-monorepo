8b898431472c8376cc11220651ed0a5bae095a3a
rzadp <rzadp@student.mini.pw.edu.pl>
2019-10-14 21:31:14 +0200

    Started introducing test.sh for testing trained weights

---
+++ badminton/training/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd $(dirname $0)
+set -e
+cd ..
+
+for f in training/logs/*/
+do
+  LOG_FOLDER=$(basename "$f" .env)
+  WEIGHTS=$(ls "./$f" | grep .h5)
+  WEIGHTS=${f}$WEIGHTS
+  DATASET=$(echo $(basename "$f") | cut -f 1-2 -d "_")
+
+  for img in datasets/$DATASET/test/*
+  do
+    echo "running for $img"
+    OUTPUT=${f}$(basename $img)
+    ./detection/detect.sh \
+      $(realpath "$WEIGHTS") \
+      $(realpath "$img") \
+      $(realpath "$OUTPUT")
+  done
+done
