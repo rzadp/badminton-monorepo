390da0d94744945f51c66ab66a1ace651bd3acb1
rzadp <rzadp@student.mini.pw.edu.pl>
2019-10-20 18:00:27 +0200

    checkpoints, best weights

--- badminton/training/test.sh
+++ badminton/training/test.sh
@@ -6,7 +6,7 @@ cd ..
 for f in training/logs/*/
 do
   source ${f}$(ls "./$f" | grep .env)
-  WEIGHTS=$(ls "./$f" | grep .h5)
+  WEIGHTS=$(ls "./$f" | grep best.h5)
   WEIGHTS=${f}$WEIGHTS
   echo -e "\n\n ...::: Testing for $(basename $f), mask size is ${MASK_SIZE} :::..."
   echo -e "weights are ${WEIGHTS}\n\n"
