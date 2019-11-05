4552718b6d89b59394ab187eb615733251bb9df8
rzadp <przemek@ethworks.io>
2019-11-05 21:38:28 +0100

    Script

--- thesis/scripts/mermaid.sh
+++ thesis/scripts/mermaid.sh
@@ -2,10 +2,9 @@
 set -e
 cd $(dirname $0)
 NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
-command -v node || \
-{ [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" "$NVM_OPTION" } || \
-{ echo "node not found"; exit -1; }
+command -v node || { echo "node not found"; exit -1; }
 
 cd ..
 find . -name '*.mmd' | while read file; do
